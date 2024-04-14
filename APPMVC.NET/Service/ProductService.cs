2f99be2e969dd5698ecfadb1a53fcaaa3db1de4e
tranhoangdong <88124107+tranhoangdong@users.noreply.github.com>
2024-04-14 19:10:14 +0700

    14/4/2024 video1

---
+++ APPMVC.NET/Service/ProductService.cs
@@ -0,0 +1,25 @@
+﻿using APPMVC.NET.Models;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APPMVC.NET.Service
+{
+    public class ProductService : List<ProductModel>
+    {
+        public ProductService()
+        {
+            this.AddRange(new ProductModel[]
+                {
+                    new ProductModel() { Id = 1,Name = "IphoneX", Price = 1000},
+                    new ProductModel() { Id = 2,Name = "SamSung", Price = 500},
+                    new ProductModel() { Id = 3,Name = "Sony", Price = 800},
+                    new ProductModel() { Id = 4,Name = "Nokia", Price = 100}
+
+                });
+
+        }
+    }
+}
--- APPMVC.NET/Startup.cs
+++ APPMVC.NET/Startup.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using APPMVC.NET.Service;
+
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 using System;
 using System.Collections.Generic;
@@ -14,9 +18,11 @@ namespace APPMVC.NET
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public static string ContentRootPath { get; set; }
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
             Configuration = configuration;
+            ContentRootPath = env.ContentRootPath;
         }
 
         public IConfiguration Configuration { get; }
@@ -25,6 +31,21 @@ namespace APPMVC.NET
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddRazorPages();
+            //services.AddTransient(typeof(ILogger<>), typeof(ILogger<>));
+            services.Configure<RazorViewEngineOptions>(options =>
+            {
+                //view/controller/action.cshtml
+                //myview/controller/action.cshtml
+
+                //{0} -> ten Action
+                //{1} -> ten Controller
+                //{3} -> ten Area
+                options.ViewLocationFormats.Add("/MyView/{1}/{0}" + RazorViewEngine.ViewExtension);
+            });
+
+            services.AddSingleton(typeof(ProductService), typeof(ProductService));
+               
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,20 +60,23 @@ namespace APPMVC.NET
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
 
-            app.UseAuthentication(); //xác định danh tính
-            app.UseAuthorization();
+            app.UseAuthentication(); //xác định danh tính 
+            app.UseAuthorization();  // xác thực quyền truy cập
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapRazorPages();
             });
         }
     }
